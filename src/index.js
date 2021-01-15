f249550e9c7928157e6dbdf88864378f0b9020ed
Kevin Zhou <kevinzhou22@outlook.com>
2021-01-15 14:46:14 +1100

    Import all the necessary files in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+import './reset.css';
 import './style.css';
+import './modules/presenter';
+import '@fortawesome/fontawesome-free/css/solid.min.css';
+import '@fortawesome/fontawesome-free/css/fontawesome.css';
+
+require.context('./images', false, /\.(png|svg|jpg|jpeg|gif)$/);
